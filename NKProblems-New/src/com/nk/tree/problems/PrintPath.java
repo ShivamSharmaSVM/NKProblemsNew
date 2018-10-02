c51158c6ab5be5fc03c5a214e60cc7a334c4b41d
shivamsharmasvm <shivamsharmasvm20@gmail.com>
2018-10-02 08:26:08 +0530

    Problem-20 : Given a binary tree, print out all of its root-to-leaf paths.(Iterative)

---
+++ NKProblems-New/src/com/nk/tree/problems/PrintPath.java
@@ -0,0 +1,45 @@
+/*
+ * Problem-20 : Given a binary tree, print out all of its root-to-leaf paths.
+ */
+
+package com.nk.tree.problems;
+
+import java.util.Stack;
+
+import com.nk.tree.core.MyBinaryTree;
+import com.nk.tree.core.Node;
+
+public class PrintPath
+{
+	public static void main(String[] args)
+	{
+		printPath(MyBinaryTree.getComplexBinaryTree());
+	}
+	
+	public static void printPath(MyBinaryTree tree)
+	{
+		Node current = tree.getRoot();
+		Stack<Node> stack = new Stack<>();
+		while(current!=null || !stack.isEmpty())
+		{
+			if(current!=null)
+			{
+				stack.push(current);
+				current = current.getLeftNode();
+			}
+			else if(stack.peek().hasRightNode())
+			{
+				current = stack.peek().getRightNode();
+			}
+			else
+			{
+				System.out.println(stack);
+				Node temp = stack.pop();
+				while(!stack.isEmpty() && stack.peek().getRightNode() == temp)
+				{
+					temp = stack.pop();
+				}
+			}
+		}
+	}
+}
